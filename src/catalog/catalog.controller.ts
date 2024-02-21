31b61fac406ec2a4cc0ee86ad172bb6f940d0426
rois <roi@saturn72.com>
2024-02-21 17:25:14 +0200

    remove redundant code

--- src/catalog/catalog.controller.ts
+++ src/catalog/catalog.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { CatalogService } from './catalog.service';
 
 @Controller('catalog')
