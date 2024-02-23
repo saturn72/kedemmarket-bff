b50a17e3f1d0b80d507a2abfbc287dde391ad79e
rois <roi@saturn72.com>
2024-02-23 15:47:10 +0200

    add caching attributes

--- src/catalog/catalog.controller.ts
+++ src/catalog/catalog.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { CatalogService } from './catalog.service';
+import { CacheKey, CacheTTL } from '@nestjs/cache-manager';
 
 @Controller('catalog')
 export class CatalogController {
-  constructor(private readonly catalogService: CatalogService) {}
+  constructor(private readonly catalogService: CatalogService) { }
 
-  @Get()
-  findAll() {
+  @CacheKey('catalog')
+  @CacheTTL(60 * 5)
+  @Get() findAll() {
     return this.catalogService.findAll();
   }
 }
