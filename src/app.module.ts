b14ef986f5c60a6efa64508103b4dd211b61380c
rois <roi@saturn72.com>
2024-02-27 20:03:05 +0200

    remove redundant code

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { CacheModule } from '@nestjs/cache-manager';
 import { CatalogModule } from './catalog/catalog.module';
 import { ConfigModule } from '@nestjs/config';
 import { CoreModule } from './core/core.module';
-import corsConfig from '@config/cors';
 import firebaseConfig from '@config/firebase';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { SseCacheExclusionHttpInterceptor } from './core/interceptors/CacheExclusionHttpInterceptor';
@@ -18,7 +17,7 @@ import { AppGateway } from './core/gateways/app.gateway';
     }),
     CatalogModule,
     ConfigModule.forRoot({
-      load: [corsConfig, firebaseConfig],
+      load: [firebaseConfig],
       isGlobal: true,
       ignoreEnvFile: false,
     }),
