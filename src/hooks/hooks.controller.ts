3668fa240bd0606a9ef04d26aefe469d88873d1d
rois <roi@saturn72.com>
2024-03-03 23:14:28 +0200

    add logging to hooks

--- src/hooks/hooks.controller.ts
+++ src/hooks/hooks.controller.ts
@@ -1,12 +1,15 @@
-import { Body, Controller, Put } from '@nestjs/common';
+import { Body, Controller, Logger, Put } from '@nestjs/common';
 import { EmitService } from 'src/core/services/emit.service';
 
 @Controller('hooks')
 export class HooksController {
-  constructor(private emit: EmitService) {}
+  private logger: Logger = new Logger(HooksController.name);
+
+  constructor(private emit: EmitService) { }
 
   @Put()
   catalogUpdated(@Body() body: { key: string; payload: any }) {
+    this.logger.log("web-hook was triggered: " + JSON.stringify(body));
     this.emit.server.emit(body.key, body.payload);
   }
 }
